a3634b2881f15387ff7ee9877eec9f2627002816
satyajit-pradhan522 <satyajitpradhan522@gmail.com>
2025-10-12 15:28:59 +0530

    worldatlas project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import './App.css'
+
+const Home = React.lazy(() => import("./pages/Home"));
+const About = React.lazy(() => import("./pages/About"));
+const Country = React.lazy(() => import("./pages/Country"));
+const Contact  = React.lazy(() => import("./pages/Contact"));
 
 function App() {
 
   return (
     <>
-      
+      <Navbar/>
+      <main className="max-w-6xl mx-auto p-4">
+        <Suspense fallback={<p className="p-4">Loading page…</p>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/country" element={<Country />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<p>404 Not Found</p>} />
+          </Routes>
+        </Suspense>
+      </main>
+      <Footer/>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { FaLongArrowAltRight } from 'react-icons/fa'
 
 function Home() {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <main className='hero-section main'>
+        <div className='container grid grid-two-cols'>
+            <div className='hero-content'>
+                <h1 className='heading-xl'>
+                    Explore the World, One Country at a Time.
+                </h1>
+                <p className='paragraph'>
+                    Discover the history, culture, and beauty of every nation. Sort, search, and filter through countries to find the details you need.
+                </p>
+                <button className='btn btn-darken btn-inline bg-white-box'>
+                    Start Exploring <FaLongArrowAltRight/>
+                </button>
+            </div>
+            <div className='hero-image'>
+                <img src="src\assets\world.png" alt="world is beauty" className='banner-image' />
+            </div>
+        </div>
+    </main>
   )
 }
 
